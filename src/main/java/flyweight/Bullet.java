7eda2e6e870fc609c3119e3adc1cbb16525c691f
darkant99 <darkant99@naver.com>
2022-05-22 20:32:09 +0900

    [FEAT] 3. 팩토리 메서드를 사용해 인스턴스 생성을 조절 합니다.

--- src/main/java/flyweight/Bullet.java
+++ src/main/java/flyweight/Bullet.java
@@ -1,6 +1,11 @@
 package flyweight;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bullet {
+    private static final Map<String, Bullet> cached = new HashMap<>();
+
     /** 1. 불변성을 지닌 내부 속성들 */
     private final String name;
     private final int damage;
@@ -11,5 +16,13 @@ public class Bullet {
         this.name = name;
         this.damage = damage;
         this.speed = speed;
+    }
+
+    /** 3. 팩토리 메서드를 사용해 인스턴스 생성을 조절 합니다. */
+    public static Bullet of(String name, int damage, int speed) {
+        if (!cached.containsKey(name)) {
+            cached.put(name, new Bullet(name, damage, speed));
+        }
+        return cached.get(name);
     }
 }
